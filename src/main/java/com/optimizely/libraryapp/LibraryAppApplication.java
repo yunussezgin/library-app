f2733e503e1406368e667f07b5ad648234250952
Yunus Sezgin <yunussezgin8@hotmail.com>
2022-04-01 09:59:40 +0300

    Print console responses

--- src/main/java/com/optimizely/libraryapp/LibraryAppApplication.java
+++ src/main/java/com/optimizely/libraryapp/LibraryAppApplication.java
@@ -2,6 +2,7 @@ package com.optimizely.libraryapp;
 
 import java.util.List;
 
+import org.apache.commons.collections.CollectionUtils;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
@@ -21,30 +22,48 @@ public class LibraryAppApplication {
 	public static void main(String[] args) {
 		ApplicationContext applicationContext = SpringApplication.run(LibraryAppApplication.class, args);
 
+		// FUNCTIONALITY REQUERMENTS
+
+		// AUTHOR
 		AuthorService authorService = applicationContext.getBean(AuthorService.class);
 		List<Author> authorList = authorService.getAllAuthors();
+		printListConsole(authorList);
 
-		for (Author author : authorList)
-			System.out.println(author.toString());
-
+		// BOOK
 		BookService bookService = applicationContext.getBean(BookService.class);
 		List<Book> bookList = bookService.getAllBooks();
+		printListConsole(bookList);
 		Book bookByIsbn = bookService.getBookByIsbn("4545-8558-3232");
+		printConsole(bookByIsbn);
 		List<Book> bookByAuthor = bookService.getBooksByAuthor("pr-walter@optivo.de");
-		List<Book> bookByTitle = bookService.getBooksByTitle("p");
+		printListConsole(bookByAuthor);
+		List<Book> bookByTitle = bookService.getBooksByTitle("Das Perfekte Dinner");
+		printListConsole(bookByTitle);
 
-		for (Book book : bookList)
-			System.out.println(book.toString());
-
+		// MAGAZINE
 		MagazineService magazineService = applicationContext.getBean(MagazineService.class);
 		List<Magazine> magazineList = magazineService.getAllMagazines();
+		printListConsole(magazineList);
 		Magazine magazineByIsbn = magazineService.getMagazineByIsbn("2365-8745-7854");
+		printConsole(magazineByIsbn);
 		List<Magazine> magazineByAuthor = magazineService.getMagazinesByAuthor("pr-gustafsson@optivo.de");
-		List<Magazine> magazineByTitle = magazineService.getMagazinesByTitle("V");
+		printListConsole(magazineByAuthor);
+		List<Magazine> magazineByTitle = magazineService.getMagazinesByTitle("Gourmet");
+		printListConsole(magazineByTitle);
 
-		for (Magazine magazine : magazineList)
-			System.out.println(magazine.toString());
+	}
 
+	private static <T> void printListConsole(List<T> objList) {
+		if (CollectionUtils.isEmpty(objList))
+			return;
+		for (Object obj : objList)
+			System.out.println(obj.toString());
+	}
+
+	private static <T> void printConsole(T obj) {
+		if (obj == null)
+			return;
+		System.out.println(obj.toString());
 	}
 
 }
---
+++ src/test/java/com/optimizely/libraryapp/service/AuthorServiceTests.java
@@ -0,0 +1,5 @@
+package com.optimizely.libraryapp.service;
+
+public class AuthorServiceTests {
+
+}
